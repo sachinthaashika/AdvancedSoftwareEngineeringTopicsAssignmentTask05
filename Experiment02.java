ecefadbecb84707ff12264db8e0567c45b921599
sachinthaashika <86411401+sachinthaashika@users.noreply.github.com>
2022-07-06 22:20:22 +0530

    Use imperative methods to store data and processing data

---
+++ Experiment02.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * @author Sachintha Ashika Bandara Yapa
+ ******************************************************************************/
+public class Experiment02 {
+	public static void main(String[] args) {
+		String[] n1 = {"Justin Bieber", "Selena Gomez", "BTS"};
+		
+		//for(int i=0;i<n1.length;i++)
+		
+		for (String name : n1) {
+			System.out.println(name);
+		}
+	}
+}
+/*
+uses a foreach loop which is less error
+
+Experiment 03
+n1 array --> n2 list
+display values of list ---> one value at a time
+*/
+
